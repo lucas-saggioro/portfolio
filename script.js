a0ea3d09bc33bc35f316bc0d6c7b01910e14a188
lucas-saggioro <xsaggioro@gmail.com>
2023-04-30 17:33:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -49,25 +49,58 @@ $(document).ready(function(){
     });
 
     // carrossel
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: false,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
+var owl = $('.carousel');
+
+function setOwlLoop() {
+  if ($(window).width() < 768) {
+    owl.trigger('destroy.owl.carousel');
+    owl.owlCarousel({
+      margin: 20,
+      loop: true,
+      autoplay: true,
+      autoplayTimeOut: 2000,
+      autoplayHoverPause: true,
+      responsive: {
+        0:{
+          items: 1,
+          nav: false
+        },
+        600:{
+          items: 2,
+          nav: false
+        },
+        1000:{
+          items: 3,
+          nav: false
         }
+      }
     });
+  } else {
+    owl.trigger('destroy.owl.carousel');
+    owl.owlCarousel({
+      margin: 20,
+      loop: false,
+      autoplay: true,
+      autoplayTimeOut: 2000,
+      autoplayHoverPause: true,
+      responsive: {
+        0:{
+          items: 1,
+          nav: false
+        },
+        600:{
+          items: 2,
+          nav: false
+        },
+        1000:{
+          items: 3,
+          nav: false
+        }
+      }
+    });
+  }
+}
+
+$(document).ready(setOwlLoop);
+$(window).resize(setOwlLoop);
 });
